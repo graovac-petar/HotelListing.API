2ff6b59c720f15043f9fb56f09cda22dcd750984
graovac-petar <petargraovac01@gmail.com>
2023-09-15 18:56:51 +0200

    Added EntityFramework and Created Database

---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id=1,
+                    Name="Serbia",
+                    ShortName="SER"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Montenegro",
+                    ShortName = "MNE"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Bosnia",
+                    ShortName = "BIH"
+                }
+            );
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Kop",
+                    Address = "Kopaonik",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "HN",
+                    Address = "Herceg Novi",
+                    CountryId = 2,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "BL",
+                    Address = "Banja Luka",
+                    CountryId = 3,
+                    Rating = 4
+                }
+            );
+        }
+    }
+}
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,8 +1,16 @@
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+builder.Services.AddDbContext<HotelListingDbContext>(options =>{
+    options.UseSqlServer(connectionString);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
