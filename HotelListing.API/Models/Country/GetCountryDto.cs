22008245bcfb605b934a71615253f81466108de0
graovac-petar <petargraovac01@gmail.com>
2023-09-17 16:13:43 +0200

    Rest API Development Best Practises. Automapper and Repo

---
+++ HotelListing.API/Models/Country/GetCountryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class GetCountryDto : BaseCountryDto
+    {
+        public int Id { get; set; }
+
+    }
+
+
+
+
+}
--- HotelListing.API/Program.cs
+++ HotelListing.API/Program.cs
@@ -1,14 +1,17 @@
+using HotelListing.API.Configuration;
+using HotelListing.API.Contracts;
 using HotelListing.API.Data;
+using HotelListing.API.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
-using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 var connectionString = builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
-builder.Services.AddDbContext<HotelListingDbContext>(options =>{
+builder.Services.AddDbContext<HotelListingDbContext>(options =>
+{
     options.UseSqlServer(connectionString);
 });
 
@@ -17,16 +20,22 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options => {
-    options.AddPolicy("AllowAll", 
-        b=>b.AllowAnyHeader()
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyHeader()
             .AllowAnyOrigin()
             .AllowAnyMethod());
 });
 
-builder.Host.UseSerilog((ctx,lc) => 
+builder.Host.UseSerilog((ctx, lc) =>
     lc.WriteTo.Console().
     ReadFrom.Configuration(ctx.Configuration));
+
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository, CountriesRepository>();
 
 var app = builder.Build();
 
